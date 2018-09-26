152d9e3a2d7d5b788945bc9808136f0bcfb7b617
Thomas Kercheval <spacekattpoispin@gmail.com>
2018-09-26 14:56:20 -0700

    Add POST route; next step database

--- src/main.py
+++ src/main.py
@@ -10,19 +10,30 @@ ROUTES = web.RouteTableDef()
 @ROUTES.get('/')
 async def root_handle(req):
     '''
-    REEE
+    Handles a GET request to root
     '''
-    return web.Response(text='Get OUTTA here...\n')
+    return web.Response(text=str(req.headers) + '\n')
 
 
-@ROUTES.get('/{name}')
+@ROUTES.get('/name/{name}')
 async def name_handle(req):
     '''
-    REee
+    Handles other routes
     '''
     name = req.match_info.get('name', 'Anon')
     text = 'Hello, ' + name + '\n'
     return web.Response(text=text)
+
+
+@ROUTES.put('/name/{name}')
+async def name_post(req):
+    '''
+    Testing errors
+    '''
+    name = req.match_info.get('name', False)
+    if name is False:
+        return web.Response(status=400)
+    return web.Response()
 
 if __name__ == '__main__':
     APP = web.Application()
