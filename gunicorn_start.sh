5e8ca80b96744261694318d59768e02fe4e509d8
Thomas Kercheval <spacekattpoispin@gmail.com>
2018-09-30 21:52:48 -0700

    Update gunicorn deploy

--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+source .env
+
 cd src
-gunicorn --bind 0.0.0.0:8080 --worker-class aiohttp.GunicornWebWorker main:APP
+gunicorn --bind 0.0.0.0:8080 \
+         --worker-class aiohttp.GunicornUVLoopWebWorker \
+         --workers=9 \
+         main:APP
