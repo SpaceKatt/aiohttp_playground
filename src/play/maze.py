01dced7d8e8307a1ee2adc24d0e08e6b27e173cc
Thomas Kercheval <spacekattpoispin@gmail.com>
2018-10-31 13:46:46 -0700

    Add paths to traverse with web crawler

--- src/play/maze.py
+++ src/play/maze.py
@@ -6,6 +6,7 @@ from aiohttp import web
 
 PREFIX = '/maze'
 ROUTES = web.RouteTableDef()
+HOST = 'http://127.0.0.1:8080'
 
 
 ROUTES.get(PREFIX)
@@ -15,14 +16,14 @@ ROUTES.get(PREFIX)
 async def handle_HEAD(req):
     return web.Response(status=200, content_type='text/html', text='''
             <html>
-            reee <a href="./maze/selector/one">Reeee</a>
+            reee <a href="{0}/maze/rotator">Reeee</a>
             </html>
-    ''')
+    '''.format(HOST))
 
 
-@ROUTES.get(PREFIX + '/selector/')
+@ROUTES.get(PREFIX + '/selector')
 async def handle_enter_maze(req):
-    return web.HTTPFound('/maze/selector/one')
+    return web.HTTPFound('{}/maze/selector/one'.format(HOST))
 
 
 @ROUTES.get(PREFIX + '/selector/{card}')
@@ -43,9 +44,37 @@ async def handle_wildcard_maze(req):
 
     return web.Response(status=200, content_type='text/html', text='''
             <html>
-            reee <a href="./">Reeee</a> <br>
-            reee <a href="./one">Reeee</a> <br>
-            reee <a href="./two">Reeee</a> <br>
-            reee <a href="./three">Reeee</a> <br>
+            reee <a href="{0}/maze/selector">Reeee</a> <br>
+            reee <a href="{0}/maze/selector/one">Reeee</a> <br>
+            reee <a href="{0}/maze/selector/two">Reeee</a> <br>
+            reee <a href="{0}/maze/selector/three">Reeee</a> <br>
             </html>
-    ''')
+    '''.format(HOST))
+
+
+@ROUTES.get(PREFIX + '/rotator')
+async def handle_selector(req):
+    return web.HTTPFound('{}/maze/rotator/one'.format(HOST))
+
+
+@ROUTES.get(PREFIX + '/rotator/{card}')
+async def handle_selector_rotate(req):
+    '''
+    Servers the same page three times
+    '''
+    linked_list = {
+            "one": "two",
+            "two": "three",
+            "three": "out"
+    }
+
+    card = req.match_info.get('card', None)
+
+    if card is None or card not in linked_list:
+        return web.HTTPFound(HOST + PREFIX + '/selector')
+
+    return web.Response(status=200, content_type='text/html', text='''
+            <html>
+            reee <a href="{0}/maze/rotator/{1}">Reeee</a> <br>
+            </html>
+    '''.format(HOST, linked_list[card]))
